psalm/endtoend-test-psl

[IO] initial commit

src/Psl/IO/CloseSeekHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekHandle extends SeekHandle, CloseHandle
+{
+}

src/Psl/IO/CloseSeekReadHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekReadHandle extends SeekReadHandle, CloseReadHandle, CloseSeekHandle
+{
+}

src/Psl/IO/CloseSeekReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekReadWriteHandle extends SeekReadWriteHandle, CloseReadWriteHandle, CloseSeekWriteHandle, CloseSeekReadHandle
+{
+}

src/Psl/IO/CloseSeekWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekWriteHandle extends SeekWriteHandle, CloseWriteHandle, CloseSeekHandle
+{
+}

src/Psl/IO/CloseWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseWriteHandle extends WriteHandle, CloseHandle
+{
+}

src/Psl/IO/ReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface ReadWriteHandle extends WriteHandle, ReadHandle
+{
+}

src/Psl/IO/SeekReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface SeekReadWriteHandle extends ReadWriteHandle, SeekWriteHandle, SeekReadHandle
+{
+}

src/Psl/Internal/Loader.php
@@ -340,6 +340,11 @@ final class Loader
         'Psl\Xml\Internal\issue_collection_from_xml_errors',
         'Psl\Xml\Internal\issue_from_xml_error',
         'Psl\Xml\Internal\issue_level_from_xml_error',
+        'Psl\Internal\suppress',
+        'Psl\IO\Internal\open',
+        'Psl\IO\input',
+        'Psl\IO\output',
+        'Psl\IO\error',
     ];
 
     public const INTERFACES = [
@@ -402,6 +407,36 @@ final class Loader
         'Psl\Xml\Issue\IssueCollection',
         'Psl\Xml\Issue\Level',
         'Psl\Xml\Exception\RuntimeException',
+        'Psl\Json\Exception\JsonDecodeException',
+        'Psl\Json\Exception\JsonEncodeException',
+        'Psl\IO\Handle',
+        'Psl\IO\SeekHandle',
+        'Psl\IO\CloseHandle',
+        'Psl\IO\ReadHandle',
+        'Psl\IO\WriteHandle',
+        'Psl\IO\SeekReadHandle',
+        'Psl\IO\CloseSeekReadHandle',
+        'Psl\IO\CloseWriteHandle',
+        'Psl\IO\CloseSeekWriteHandle',
+        'Psl\IO\ReadWriteHandle',
+        'Psl\IO\CloseReadWriteHandle',
+        'Psl\IO\SeekReadWriteHandle',
+        'Psl\IO\CloseSeekReadWriteHandle',
+    ];
+
+    public const TRAITS = [
+        'Psl\Type\Internal\TypeTraceTrait',
+    ];
+
+    public const CLASSES = [
+        'Psl\Exception\InvariantViolationException',
+        'Psl\Iter\Iterator',
+        'Psl\Collection\Vector',
+        'Psl\Collection\MutableVector',
+        'Psl\Collection\Map',
+        'Psl\Collection\MutableMap',
+        'Psl\Asio\WrappedException',
+        'Psl\Asio\WrappedResult',
     ];
 
     private const TYPE_CONSTANTS = 1;

BackEndTea/psl

src/Psl/IO/CloseSeekHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekHandle extends CloseHandle, SeekHandle
+{
+}

src/Psl/IO/CloseSeekReadHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekReadHandle extends CloseReadHandle, CloseSeekHandle, SeekReadHandle
+{
+}

src/Psl/IO/CloseSeekReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekReadWriteHandle extends CloseReadWriteHandle, CloseSeekReadHandle, CloseSeekWriteHandle, SeekReadWriteHandle
+{
+}

src/Psl/IO/CloseSeekWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseSeekWriteHandle extends CloseSeekHandle, CloseWriteHandle, SeekWriteHandle
+{
+}

src/Psl/IO/CloseWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface CloseWriteHandle extends CloseHandle, WriteHandle
+{
+}

src/Psl/IO/ReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface ReadWriteHandle extends ReadHandle, WriteHandle
+{
+}

src/Psl/IO/SeekReadWriteHandle.php
@@ -1,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Psl\IO;
+
+interface SeekReadWriteHandle extends ReadWriteHandle, SeekReadHandle, SeekWriteHandle
+{
+}

src/Psl/Internal/Loader.php
@@ -437,6 +437,11 @@ final class Loader
         'Psl\Encoding\Base64\decode',
         'Psl\Encoding\Hex\encode',
         'Psl\Encoding\Hex\decode',
+        'Psl\Internal\suppress',
+        'Psl\IO\Internal\open',
+        'Psl\IO\input_handle',
+        'Psl\IO\output_handle',
+        'Psl\IO\error_handle',
     ];
 
     public const INTERFACES = [
@@ -455,6 +460,23 @@ final class Loader
         'Psl\Observer\ObserverInterface',
         'Psl\Result\ResultInterface',
         'Psl\Encoding\Exception\ExceptionInterface',
+        'Psl\IO\Exception\ExceptionInterface',
+        'Psl\IO\CloseHandle',
+        'Psl\IO\CloseReadHandle',
+        'Psl\IO\CloseReadWriteHandle',
+        'Psl\IO\CloseSeekHandle',
+        'Psl\IO\CloseSeekReadHandle',
+        'Psl\IO\CloseSeekReadWriteHandle',
+        'Psl\IO\CloseSeekWriteHandle',
+        'Psl\IO\CloseWriteHandle',
+        'Psl\IO\Handle',
+        'Psl\IO\ReadHandle',
+        'Psl\IO\ReadWriteHandle',
+        'Psl\IO\SeekHandle',
+        'Psl\IO\SeekReadHandle',
+        'Psl\IO\SeekReadWriteHandle',
+        'Psl\IO\SeekWriteHandle',
+        'Psl\IO\WriteHandle',
     ];
 
     public const TRAITS = [
@@ -502,6 +524,8 @@ final class Loader
         'Psl\Hash\Context',
         'Psl\Encoding\Exception\IncorrectPaddingException',
         'Psl\Encoding\Exception\RangeException',
+        'Psl\IO\Exception\BlockingException',
+        'Psl\IO\Internal\ResourceHandle',
     ];
 
     private const TYPE_CONSTANTS = 1;